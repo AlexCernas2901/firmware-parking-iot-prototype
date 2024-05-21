82b424cc8d5e47a2b9a57491fce42bf937cf35d2
AlexCernas2901 <acernas1@ucol.mx>
2024-05-21 14:06:00 -0600

    Merge servos into main

--- app/app_data.h
+++ app/app_data.h
@@ -1,5 +1,5 @@
-#ifndef DATA_H
-#define DATA_H
+#ifndef APP_DATA_H
+#define APP_DATA_H
 
 #include "display_oled.h"
 DisplayOled displayOled; // Instanciando la clase DisplayOled
@@ -10,4 +10,8 @@ MagneticModules magneticModules; // Instanciando la clase MagneticModules
 #include "wifi.h"
 WifiConnection wifiConnection; // Instanciando la clase WifiConnection
 
+#include "servos.h"
+Servos servos; // Instanciando la clase Servos
+
 #endif
+  
--- app/leds.h
+++ app/leds.h
@@ -1,15 +1,15 @@
 class Leds {
 public:
-  const int8_t LED1_R = 4;  // GPIO 36
-  const int8_t LED1_G = 5;  // GPIO 39
-  const int8_t LED1_B = 6;  // GPIO 34
-  const int8_t LED2_R = 7;  // GPIO 35
-  const int8_t LED2_G = 8;  // GPIO 32
-  const int8_t LED2_B = 9;  // GPIO 33
-  const int8_t LED3_R = 10; // GPIO 25
-  const int8_t LED3_G = 11; // GPIO 26
-  const int8_t LED3_B = 13; // GPIO 14
-  const int8_t LED4_R = 14; // GPIO 12
-  const int8_t LED4_G = 16; // GPIO 13
-  const int8_t LED4_B = 17; // GPIO 9
+  const int8_t LED1_R = 36; // GPIO 36
+  const int8_t LED1_G = 39; // GPIO 39
+  const int8_t LED1_B = 34; // GPIO 34
+  const int8_t LED2_R = 35; // GPIO 35
+  const int8_t LED2_G = 32; // GPIO 32
+  const int8_t LED2_B = 33; // GPIO 33
+  const int8_t LED3_R = 25; // GPIO 25
+  const int8_t LED3_G = 26; // GPIO 26
+  const int8_t LED3_B = 14; // GPIO 14
+  const int8_t LED4_R = 12; // GPIO 12
+  const int8_t LED4_G = 13; // GPIO 13
+  const int8_t LED4_B = 9;  // GPIO 9
 };
